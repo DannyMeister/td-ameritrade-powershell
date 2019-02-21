daf13c29c7c8e9f7e3d777fc82f3934de84a1c68
DannyMeister <DDSquad_99@yahoo.com>
2019-02-21 01:43:13 -0600

    working New-TdUserId and New-TdAuthorizationToken

--- README.md
+++ README.md
@@ -18,12 +18,55 @@ From a PowerShell prompt, dot source the file and then you can begin calling fun
 . .\ameritrade.ps1
 ```
 
-## Features
-
-* New-UserId: Return a new OAuth User Id for use when creating a new app on the TD Ameritrade developer website.
+### Register an app
+Return a new OAuth User Id for use when creating a new app on the TD Ameritrade developer website.  You can skip this step and use an existing user id if you have already
 
 ```powershell
-PS> . .\ameritrade.ps1
-PS> New-UserId
-94a127206dd9
+New-TdUserId
+# Example response: A265B038D9C0
 ```
+
+Register an app with the following values
+
+* Application name: anyting you want
+* Description: anything you want
+* User Id: use the response from `New-TdUserId`.  For example, `A265B038D9C0`
+* Redirect URL: `https://localhost:8080/` (I will make this configurable in the future.)
+
+### Login
+
+```powershell
+$response = New-TdAuthorizationToken -RedirectUrl "https://localhost:8080/" -ClientId "A265B038D9C0"
+```
+
+The script will pop open a browser to the TD Ameritrade login page.  Log in with your brokerage account credentials.
+
+After successful login, the page will redirect to https://localhost:8080 which is being temporarily hosted by the PowerShell command. It will gather the authorization code and make the necessary subsequent request to retrieve the access token and refresh token. The tokens you need to interact with the API are available on the page, and returned in the response to PowerShell.
+
+![redirect page screenshot](/docs/redirect.png)
+
+Your bearer token is now accessible in `$response.access_token`.
+
+Your refresh token is now accessible in `$response.refresh_token`.
+
+You may want to save these somewhere easily accessible in the future for your program/automated scripts to use without requiring user interactivity.  Perhaps to an encrypted file via `ConvertFrom-SecureString` and `Set-Content`.
+
+### Call TD Ameritrade API
+
+```powershell
+# query Walmart (WMT) stock quote
+$apiResponse = Invoke-WebRequest "https://api.tdameritrade.com/v1/marketdata/WMT/quotes" -Headers @{Authorization="Bearer $($response.access_token)"}
+
+# format the info
+$apiResponse.Content | ConvertFrom-Json | select -expand WMT
+```
+
+![Walmart stock quote response screenshot](/docs/WMT.png)
+
+<sub>Data blurred because the developer agreement says not to share data retrieved by API.</sub>
+
+### Get new access token via refresh token
+
+When the access token expires, you can fetch a new one via the refresh token without having to log in again.
+
+TODO: finish refresh token code and doc
--- ameritrade.ps1
+++ ameritrade.ps1
@@ -1,3 +1,139 @@
-function New-UserId {
-    [System.Guid]::NewGuid() | %{$_ -replace "-",""} | %{ $_.Substring(0,12) }
+function New-TdUserId {
+    [System.Guid]::NewGuid() | %{$_ -replace "-",""} | %{ $_.Substring(0,12).ToUpper() }
+}
+
+function New-TdAuthorizationToken {
+    Param(
+        # Redirect URL for the app
+        [Parameter(Mandatory=$true)]
+        [string]
+        $RedirectUrl,
+        
+        # OAuth client id for the app
+        [Parameter(Mandatory=$true)]
+        [string]
+        $ClientId
+    )
+
+    Add-Type -AssemblyName System.Web
+
+    $Parameters = @{
+        response_type = 'code'
+        redirect_uri = $RedirectUrl
+        client_id = "$ClientId@AMER.OAUTHAP"
+    }
+
+    $query = ($Parameters.Keys | %{ "$($_)=$([System.Uri]::EscapeDataString($Parameters[$_]))"}) -join "&"
+
+
+    # Root CA
+    $rootCert = New-SelfSignedCertificate -CertStoreLocation cert:\CurrentUser\My -DnsName "RootCA" -keyusage CertSign,CRLSign -TextExtension @("2.5.29.37={text}1.3.6.1.5.5.7.3.2,1.3.6.1.5.5.7.3.1,1.3.6.1.5.5.7.3.4,1.3.6.1.5.5.7.3.3,1.3.6.1.5.5.7.3.8,1.3.6.1.5.5.7.3.1","2.5.29.19={critical}{text}ca=TRUE")
+    [System.Security.SecureString]$rootcertPassword = ConvertTo-SecureString -String "password" -Force -AsPlainText
+    [String]$rootCertPath = Join-Path -Path 'cert:\CurrentUser\My\' -ChildPath "$($rootcert.Thumbprint)"
+    Export-PfxCertificate -Cert $rootCertPath -FilePath 'RootCA.pfx' -Password $rootcertPassword | Out-Null
+    Export-Certificate -Cert $rootCertPath -FilePath 'RootCA.crt' | Out-Null
+    # ssl cert
+    $testCert = New-SelfSignedCertificate -CertStoreLocation Cert:\LocalMachine\My -DnsName "localhost" -KeyExportPolicy Exportable -KeyLength 2048 -KeyUsage DigitalSignature,KeyEncipherment -textextension @("2.5.29.37={text}1.3.6.1.5.5.7.3.1") -Signer $rootCert
+    [String]$testCertPath = Join-Path -Path 'cert:\LocalMachine\My\' -ChildPath "$($testCert.Thumbprint)"
+    # import CA into trusted root
+    Import-PfxCertificate -FilePath "$PWD\RootCA.pfx" -CertStoreLocation Cert:\LocalMachine\Root -Confirm:$false -Password $rootcertPassword | Out-Null
+
+    # remove CA from My
+    Remove-Item -Force "cert:\CurrentUser\My\$($rootCert.Thumbprint)" | Out-Null
+
+    $appid = [System.Guid]::NewGuid().Guid
+    $hash = $testCert.Thumbprint
+    netsh http delete sslcert hostnameport=localhost:8080 | Out-Null
+    netsh http add sslcert hostnameport=localhost:8080 certhash=$hash appid=`{$appid`} certstorename=my | Out-Null
+
+    $listener = New-Object -TypeName System.Net.HttpListener
+    $listener.Prefixes.Add("https://localhost:8080/")
+    $listener.Start()
+
+    Write-Host "Launching browser.  Please log in to TD Ameritrade with your brokerage account credentials."
+    start "https://auth.tdameritrade.com/auth?$query"
+
+    $task = $listener.GetContextAsync();
+    while( -not $context )
+    {
+        if( $task.Wait(500) )
+        {
+            $context = $task.Result
+        }
+        sleep -Milliseconds 100;
+    }
+    $redirectRequestUrl = $context.Request.Url
+    $redirectRequestUrl | Write-Host
+    $content = [System.Text.Encoding]::UTF8.GetBytes("
+    <!doctype html>
+    <html lang='en'>
+        <head>
+            <meta charset=""utf-8"">
+            <meta name=""viewport"" content=""width=device-width, initial-scale=1, shrink-to-fit=no"">
+            <link rel=""stylesheet"" href=""https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"" integrity=""sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"" crossorigin=""anonymous"">
+            <title>TD Ameritrade Login Redirect Landing Page</title>
+        </head>
+        <body>
+            <div class=""container"">
+                <div class=""jumbotron container"">
+                    <h1 class=""display-4"">TD Ameritrade API Login Redirect Page</h1>
+                    <p class=""lead"">Served locally by temporary web host in PowerShell.</p>
+                </div>
+                <div class=""card mb-3"">
+                    <h5 class=""card-header"">Recieved the following request from TD Ameritrade login</h5>
+                    <div class=""card-body"">
+                        <pre><code>$($redirectRequestUrl.AbsoluteUri)</code></pre>
+                    </div>
+                </div>")
+    $context.Response.ContentType = "text/html"
+    $context.Response.OutputStream.Write($content, 0, $content.Length)
+
+
+    $code = [System.Web.HttpUtility]::ParseQueryString($redirectRequestUrl.Query)['code']
+
+    $tokenParameters = @{
+        grant_type = 'authorization_code'
+        access_type = 'offline'
+        code = $code
+        client_id = "$ClientId@AMER.OAUTHAP"
+        redirect_uri = $RedirectUrl
+    }
+
+    $tokenUrl = "https://api.tdameritrade.com/v1/oauth2/token"
+
+    $refreshTokenResponse = Invoke-WebRequest -ContentType "application/x-www-form-urlencoded" -UseBasicParsing -Uri $tokenUrl -Method Post -body $tokenParameters
+
+    $outputValue = $refreshTokenResponse.Content | ConvertFrom-Json
+
+    $content = [System.Text.Encoding]::UTF8.GetBytes("
+                <div class=""card mb-3"">
+                    <h5 class=""card-header"">Retrieved access token and refresh token</h5>
+                    <div class=""card-body"">
+                        <p class=""card-text"">This content was also provided in the return value of the powershell function</p>
+                        <pre><code>$($refreshTokenResponse.Content)</code></pre>
+                    </div>
+                </div>
+                <div class=""card mb-3"">
+                    <h5 class=""card-header"">Your current Authorization header for API requests</h5>
+                    <div class=""card-body"">
+                        <pre><code>Authorization : Bearer $($outputValue.access_token)</code></pre>
+                    </div>
+                </div>
+            </div>
+            <script src=""https://code.jquery.com/jquery-3.3.1.slim.min.js"" integrity=""sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"" crossorigin=""anonymous""></script>
+            <script src=""https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"" integrity=""sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1"" crossorigin=""anonymous""></script>
+            <script src=""https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"" integrity=""sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM"" crossorigin=""anonymous""></script>
+        </body>
+    </html>")
+
+    $context.Response.OutputStream.Write($content, 0, $content.Length)
+    $context.Response.OutputStream.Close()
+    $context.Response.Close();
+    sleep -Seconds 5
+
+    $listener.Stop()
+    $listener.Close()
+    $listener.Dispose()
+
+    return $refreshTokenResponse.Content | ConvertFrom-Json
 }
